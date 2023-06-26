4f1d0d1623cc123a794c43d9432c4d34ba34de04
Kamalesh <kamalesh5112000@pec.edu.com>
2023-06-27 01:53:14 +0530

    first commit

--- routes/chat.js
+++ routes/chat.js
@@ -2,27 +2,57 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 let datas ="no data found";
-let username="";
-if (fs.existsSync('message.txt')){
-  fs.readFile('message.txt', 'utf8',(err,data)=>{
-    if (err) {
-      console.error(err);
-    }else{
-      console.log(data)
-      datas=data;
-
-    }
-  });
-    
-}
 
 router.get('/', (req, res, next) => {
-    res.send(`<h3>${datas}</h3>br<form>onsumit="username=localStorage.setItem('username')"action="/add" method="POST"><input id="msg" type="text" name="msg"><button type="submit">Send</button></form>`);
+  if (fs.existsSync('message.txt')){
+    fs.readFile('message.txt', 'utf8',(err,data)=>{
+      if (err) {
+        console.error(err);
+      }else{
+        console.log(data)
+        datas=data;
+        res.send(`<h3>${datas}<h3>
+        <form 
+        onsubmit="document.getElementById('username').value=localStorage.getItem('username')" 
+        action="/add" method="POST">
+        <input id="msg" type="text" name="msg">
+        <input id="username" type="hidden" name="username">
+        <button type="submit">Send</button>
+        </form>`);
+  
+      }
+    });
+      
+  }else{
+    res.send(`<h3>${datas}<h3>
+        <form 
+        onsubmit="document.getElementById('username').value=localStorage.getItem('username')" 
+        action="/add" method="POST">
+        <input id="msg" type="text" name="msg">
+        <input id="username" type="hidden" name="username">
+        <button type="submit">Send</button>
+        </form>`);
+  }
+  
+  
+    
 });
 router.post('/add', (req, res, next) => {
-  console.log(req.body.msg);
-  console.log(username)
-  res.redirect('/');
+  
+  let tmsg=req.body.msg
+  let un=req.body.username
+  
+  console.log(req.body)
+
+    
+  fs.writeFile('message.txt',`${un}:${tmsg}`,{flag:'a'},(err)=>{
+    if(err){
+      console.log(err)
+    }else{
+      res.redirect('/');
+    }
+  })
+
 });
 
 module.exports=router;
